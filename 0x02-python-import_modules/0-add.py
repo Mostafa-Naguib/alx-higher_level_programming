204544836a6c2694e4306262f57ae473bb63eb48
mostafa <mostafanaguib200@gmail.com>
2024-03-15 00:21:48 +0200

    test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("1 + 2 = {}".format(add(a,b)))
+    print("{} + {} = {}".format(a, b, add(a,b)))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-def main():
-    sum = add(10, 5)
-    difference = sub(10, 5)
-    product = mul(10, 5)
-    quotient = div(10, 5)
-
-
-    print("10 + 5 = {:d}".format(sum))
-    print("10 - 5 = {:d}".format(difference))
-    print("10 * 5 = {:d}".format(product))
-    print("10 / 5 = {:d}".format(quotient))
-
 if __name__ == "__main__":
-    main()
+    a = 10
+    b = 5
+    sum = add(a, b)
+    difference = sub(a, b)
+    product = mul(a, b)
+    quotient = div(a, b)
+
+
+    print("{} + {} = {:d}".format(a, b, sum))
+    print("{} - {} = {:d}".format(a, b, difference))
+    print("{} * {} = {:d}".format(a, b, product))
+    print("{} / {} = {:d}".format(a, b, quotient))
