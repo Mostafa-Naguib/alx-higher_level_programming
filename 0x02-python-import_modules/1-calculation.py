e9560aa90b985bc4b028a77e803f0664b047315a
mostafa <mostafanaguib200@gmail.com>
2024-03-15 00:26:59 +0200

    test

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    """This is a test for calculator_1 module"""
     a = 10
     b = 5
     sum = add(a, b)
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+
 def main():
     list = sys.argv
     num = len(list) - 1
