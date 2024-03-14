2cf0de2ff534fd48833db9ea9f37bff10129a6db
mostafa <mostafanaguib200@gmail.com>
2024-03-15 00:11:05 +0200

    testing

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name == "__main__":
     from add_0 import add
-    def main():
-        sum = add(1, 2)
-        print("1 + 2 = {}".format(sum))
+    sum = add(1, 2)
+    print("1 + 2 = {}".format(sum))
