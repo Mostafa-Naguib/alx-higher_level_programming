9f02ed4ffb761e588b48b9de6ee24ba9a5d8fc11
Mostafa-Naguib <mostafa-naguib@kali.Mostafa>
2024-03-19 03:29:37 +0200

    fixing task 0

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return None
-    elif len(my_list) < idx:
+    elif len(my_list) <= idx:
         return None
 
     my_list[idx] = element
