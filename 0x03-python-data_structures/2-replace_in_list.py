efd070f567cff9cdb1bdbd13f979893f1e4544af
Mostafa-Naguib <mostafa-naguib@kali.Mostafa>
2024-03-20 01:09:19 +0200

    testing

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     elif len(my_list) <= idx:
-        return None
+        return my_list
 
     my_list[idx] = element
     return my_list
