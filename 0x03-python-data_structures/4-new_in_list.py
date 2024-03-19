affbedd3cbe05c3b042177f5db128337a2178542
moustafanaguib200 <moustafanaguib200@gmail.com>
2024-03-19 00:28:48 +0000

    testing

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return None
+    elif len(my_list) < idx:
+        return None
+
+    new_list = my_list[:]
+    new_list[idx] = element
+
+    return new_list
