c4b9ad475323c455cddba2d79c642ac521ced9cb
Mostafa-Naguib <mostafa-naguib@kali.Mostafa>
2024-03-20 01:24:58 +0200

    test

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for i in matrix:
-        for j in i:
-            print("{:d} ".format(j), end="")
-        print()
+        if not i:
+            print()
+        for j in range(len(i)):
+            print("{:d}".format(i[j]), end=" " if j < len(i)-1 else "\n")
