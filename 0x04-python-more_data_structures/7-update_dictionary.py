e5100f543fbf5cce7f92ecc6fb2b520224401ae3
Mostafa-Naguib <mostafa-naguib@kali.Mostafa>
2024-03-21 01:24:27 +0200

    testing

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
 def update_dictionary(a_dictionary, key, value):
-    x_dictionary = a_dictionary.copy()
-    x_dictionary[key] =  value
-    return x_dictionary
+    a_dictionary.update({key: value})
+    return a_dictionary
