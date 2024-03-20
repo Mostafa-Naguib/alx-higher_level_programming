14b4c8ec4c5ca4b118496db4a2467e8794ea9eb9
Mostafa-Naguib <mostafa-naguib@kali.Mostafa>
2024-03-21 01:52:40 +0200

    testing

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,3 +9,11 @@ def best_score(a_dictionary):
             max_value = v
             max_key = k
     return k
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
