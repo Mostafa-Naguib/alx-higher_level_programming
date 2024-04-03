b4e86d422070594567f5f5be98720010e053e55f
Mostafa-Naguib <moustafanaguib200@gmail.com>
2024-04-03 02:26:22 +0200

    testing

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,5 +13,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             new_list.append(0)
-        
+
     return new_list
