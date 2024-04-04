a35477e2c10b8a9837b39372f5c07ee315ad3f93
Mostafa-Naguib <moustafanaguib200@gmail.com>
2024-04-04 03:46:33 +0200

    testing

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,4 +4,9 @@
 class Square:
     """Defines a square with its attribute"""
     def __init__(self, size):
+        """constructor.
+
+        Args:
+            size: length of a side of the square.
+        """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,13 @@
 
 class Square:
     """Defines a square with its attribute"""
-    def __init__(self, size):
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: length of a side of the square.
+
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
