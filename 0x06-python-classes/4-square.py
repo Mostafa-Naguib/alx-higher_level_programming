cb3c1eedee708941bd6245b74f384d48f68080b1
Mostafa-Naguib <moustafanaguib200@gmail.com>
2024-04-04 03:38:35 +0200

    testing

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, new_size):
+        if not isinstance(new_size, int):
+            raise TypeError("size must be an integer")
+
+        if new_size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = new_size
+    
+    def area(self):
+        return self.__size ** 2
