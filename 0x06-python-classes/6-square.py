42c53b140fc77fc160513fcfb3ce1584f8deddb7
Mostafa-Naguib <moustafanaguib200@gmail.com>
2024-04-04 04:18:43 +0200

    testing

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Defines a square with its attribute, method, getter and setter"""
-    def __init__(self, size):
+    def __init__(self, size, position):
         """Constructor.
 
             Args:
@@ -12,6 +12,7 @@ class Square:
 
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -26,7 +27,18 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = new_size
-    
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, new_value):
+        if not isinstance(new_value, tuple) and new_value[1] > 0 and new_value[0] > 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = new_value
+
     def area(self):
         return self.__size ** 2
 
