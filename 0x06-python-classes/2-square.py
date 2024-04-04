9241dc5955a729e078acdfba450e9d8a5c1ef645
Mostafa-Naguib <moustafanaguib200@gmail.com>
2024-04-04 03:42:36 +0200

    testing

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Square module"""
+
+
 class Square:
+    """Defines a square with its attribute"""
     def __init__(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
